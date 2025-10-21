87d52f7c5986b0e2150bd5043651feab6910ad43
whaozheng802-ctrl <whaozheng802@gmail.com>
2025-10-21 09:44:44 +0800

    3333
    333

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
 
 void public MyMethod()
@@ -7,8 +7,3 @@ void public MyMethod()
     Console.WriteLine("This is my method.");
 }
 
-void public MyMethod111()
-{
-    // This is a sample method
-    Console.WriteLine("This is my method.");
-}
